5ace56c2d0734680972f364e7e41e3f1475e576d
Ivan Jovanovikj <ivan.jovanovikj@sn-invent.de>
2024-05-14 18:41:40 +0200

    feat: add the generated project

---
+++ src/main/java/com/example/demodevspaceapp/DemodevspaceappApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodevspaceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodevspaceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodevspaceappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demodevspaceapp/DemodevspaceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodevspaceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodevspaceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
